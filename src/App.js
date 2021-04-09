105b38675380bf3cac60c6ac1796af530d35d8c5
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-09 10:59:22 -0600

    add react router  and work exampls page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.scss";
 import { HeaderComponent } from "./components/HeaderComponent";
 import { HomeComponent } from "./components/home/HomeComponent";
+import { WorkPageComponent } from "./components/workpage/WorkPageComponent";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HeaderComponent />
-      <HomeComponent />
+      <Router>
+        <HeaderComponent />
+        <Switch>
+          <Route exact path="/" component={HomeComponent} />
+          <Route path="/work" component={WorkPageComponent} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/workpage/WorkPageComponent.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import "./workPageComponentStyle.scss";
+
+import item1 from "../../assets/portfolio-item1.jpg";
+import item2 from "../../assets/portfolio-item2.jpg";
+import item3 from "../../assets/portfolio-item3.jpg";
+import item4 from "../../assets/portfolio-item4.jpg";
+import item5 from "../../assets/portfolio-item5.jpg";
+import item6 from "../../assets/portfolio-item6.jpg";
+import item7 from "../../assets/portfolio-item7.jpg";
+import item8 from "../../assets/portfolio-item8.jpg";
+import item9 from "../../assets/portfolio-item9.jpg";
+import item10 from "../../assets/portfolio-item10.jpg";
+import item11 from "../../assets/portfolio-item11.jpg";
+import item12 from "../../assets/portfolio-item12.jpg";
+import item13 from "../../assets/portfolio-item13.jpg";
+import item14 from "../../assets/portfolio-item14.jpg";
+import item15 from "../../assets/portfolio-item15.jpg";
+import item16 from "../../assets/portfolio-item16.jpg";
+
+export const WorkPageComponent = () => (
+  <section>
+    <div class="title">
+      <h1>Some of our finest work</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+        iusto molestias accusamus rem nobis est, et laborum harum doloremque
+        nemo non, minima quos totam labore maxime iste alias ut esse? Repellat
+        eaque reiciendis laboriosam corporis fuga vero ratione veritatis
+        numquam, libero magni voluptatum omnis molestiae officiis exercitationem
+        qui quidem nam.
+      </p>
+    </div>
+
+    <div class="portfolio">
+      <div class="item">
+        <img src={item1} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item2} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item3} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item4} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item5} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item6} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item7} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item8} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item9} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item10} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item11} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item12} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item13} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item14} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item15} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+      <div class="item">
+        <img src={item16} alt="" />
+        <div class="action">
+          <a href="#">Launch</a>
+        </div>
+      </div>
+    </div>
+  </section>
+);
